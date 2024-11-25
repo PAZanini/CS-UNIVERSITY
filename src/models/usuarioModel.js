58cbc072764a7d4f180c922878584906d2ac660f
PAZanini <pedro.zanini@sptech.school>
2024-11-25 04:05:01 -0300

    adicionando mais conteudo na home e concertando api

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -60,13 +60,6 @@ function cadastrar(nick, email, senha) {
 }
 
 function atlGrafico(idusuario) {
-  console.log(
-    "ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",
-    nick,
-    email,
-    senha
-  );
-
   // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
   //  e na ordem de inserção dos dados.
   var instrucaoSql = `
@@ -81,4 +74,5 @@ module.exports = {
   voltaic,
   cadastrar,
   atlVoltaic,
+  atlGrafico,
 };
